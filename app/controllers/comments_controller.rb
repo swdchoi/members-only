3de57218b6493291ffe83291c407cf356615dbbe
swdchoi <sungwonchoi224@gmail.com>
2025-01-30 13:10:45 +0700

    Members only done

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,75 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[ show edit update destroy ]
+  before_action :set_post
+  # GET /comments or /comments.json
+  def index
+    @comments = Comment.all
+  end
+
+  # GET /comments/1 or /comments/1.json
+  def show
+    @comment = @post.comment.find_by(params[:id])
+  end
+
+  # GET /comments/new
+  def new
+    @comment = @post.comment.build
+  end
+
+  # GET /comments/1/edit
+  def edit
+  end
+
+  # POST /comments or /comments.json
+  def create
+    @comment = @post.comment.build(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to root_url( allow_other_host: true), notice: "Comment was successfully created." }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /comments/1 or /comments/1.json
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to post_comment_path(@comment), notice: "Comment was successfully updated." }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /comments/1 or /comments/1.json
+  def destroy
+    @comment.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to comments_path, status: :see_other, notice: "Comment was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params.expect(:id))
+    end
+
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def comment_params
+      params.expect(comment: [ :comment ])
+    end
+end
---
+++ db/migrate/20250128122609_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :posts do |t|
+      t.integer :user_id
+      t.string :header
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
